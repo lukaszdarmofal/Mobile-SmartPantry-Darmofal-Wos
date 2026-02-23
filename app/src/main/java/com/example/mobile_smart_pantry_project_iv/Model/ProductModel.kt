650f32c105c7e3dfce6bdb3985565527ef23c352
lukasz <lukasz.darmofal@cyfroweszkoly.pl>
2026-02-23 13:33:05 +0100

    Ide na obiad

---
+++ app/src/main/java/com/example/mobile_smart_pantry_project_iv/Model/ProductModel.kt
@@ -0,0 +1,12 @@
+package com.example.mobile_smart_pantry_project_iv.Model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Product (
+    val id: Int,
+    val nazwa: String,
+    val ilosc: Int,
+    val jednostka: String,
+    val kategoria: String
+)
